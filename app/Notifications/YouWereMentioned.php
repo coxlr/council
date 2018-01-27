3360258cc5e7a714ef197a2833f9f6af1a689ab1
Lee Cox <lee_cox@sky.com>
2018-01-26 18:09:59 -0800

    Fix errors from bad commit

--- app/Notifications/YouWereMentioned.php
+++ app/Notifications/YouWereMentioned.php
@@ -45,7 +45,6 @@ class YouWereMentioned extends Notification
     public function toArray($notifiable)
     {
         return [
-<<<<<<< HEAD
             'message' => $this->message(),
             'link' => $this->subject->path()
         ];
@@ -67,10 +66,3 @@ class YouWereMentioned extends Notification
         return $this->subject instanceof Reply ? $this->subject->owner : $this->subject->creator;
     }
 }
-=======
-            'message' => $this->subject['owner'].' mentioned you in '.$this->subject['title'],
-            'link' => $this->subject->path()
-        ];
-    }
-}
->>>>>>> 0147f2d6ef036623243b7aeac478a57c7d2b96e2
