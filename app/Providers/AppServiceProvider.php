b5ab8787a6eb11bc75fa07ec0ea7faee89e9db70
Lee Cox <lee_cox@sky.com>
2018-01-31 18:46:40 -0800

    Channel-dropdown fetches its own data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Channel;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,17 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Deal with the issue of migration string length on older version of mysql - LC 040617
-        Schema::defaultStringLength(191);
-
-        \View::composer('*', function ($view) {
-            $channels = \Cache::rememberForever('channels', function () {
-                return Channel::all();
-            });
-
-            $view->with('channels', $channels);
-        });
-
         \Validator::extend('spamfree', 'App\Rules\SpamFree@passes');
     }
 
